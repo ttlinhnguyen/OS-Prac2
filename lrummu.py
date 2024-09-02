4c9a7bf894a27d941b7e536410a7e669dd22d1c6
Linh Nguyen <linhtunguyen.1311@gmail.com>
2024-09-02 23:27:02 +0930

    Fix LRU

--- lrummu.py
+++ lrummu.py
@@ -1,8 +1,9 @@
 import logging
-import time
 from mmu import MMU
+from page import Page
 
 logger = logging.getLogger(__name__)
+
 
 class LruMMU(MMU):
     def __init__(self, frames):
@@ -11,10 +12,8 @@ class LruMMU(MMU):
         self.num_page_faults = 0
         self.frames = frames
 
-        self.page_table = {}  # Page number: Frame number
-        self.access_time = {}  # Last access time
-        self.memory = []  # Frame number
-        self.dirty = [] # Frame number
+        self.page_table = {}  # Page number: Page Object
+        self.memory = []  # Page number
 
     def set_debug(self):
         logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
@@ -22,75 +21,40 @@ class LruMMU(MMU):
     def reset_debug(self):
         logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.WARNING)
 
-    def track_page_in_memory(self, page_number):
-        """
-        Checks if the page is in memory and updates its access time.
-        Returns True if the page is in memory, False otherwise.
-        """
-        if page_number in self.page_table:
-            # Update access time if page is in memory
-            self.access_time[page_number] = time.time()
-            logger.debug(f"Access Page {page_number}, updated access time.")
-            return True
-        return False
-
-    def evict_page(self):
-        # Find the least recently used page
-        page = min(self.access_time, key=self.access_time.get)
-        frame = self.page_table[page]
-        logger.debug(f"Evict page {page} from frame {frame}")
-
-        # If the page is dirty - marked as written, increment disk writes
-        if frame in self.dirty:
-            self.num_disk_writes += 1
-            logger.debug(f"Write page {page} back to disk")
-
-        # Remove the LRU page from memory and tables
-        del self.page_table[page]
-        del self.access_time[page]
-        if frame in self.memory:
-            del self.memory[self.memory.index(frame)]
-
     def read_memory(self, page_number):
-        if self.track_page_in_memory(page_number):
-            # Page is in memory, no need to do anything else
-            return
-
-        # Page fault: Page is not in memory
-        self.num_page_faults += 1
-        self.num_disk_reads += 1
-        logger.debug(f"Page fault on read {page_number}")
-        if len(self.page_table) >= self.frames:
-            self.evict_page()
-
-        # Find the next available frame
-        frame_number = len(self.page_table)
-        self.page_table[page_number] = frame_number
-        self.memory.append(frame_number)
-        self.access_time[page_number] = time.time()
-        logger.debug(f"Loaded page {page_number} into frame {frame_number}")
+        self.access_memory(page_number, False)
 
     def write_memory(self, page_number):
-        if self.track_page_in_memory(page_number):
-            # Page is in memory, mark it as modified
-            frame_number = self.page_table[page_number]
-            self.memory[frame_number] = "W"
+        self.access_memory(page_number, True)
+
+    def access_memory(self, page_number, write):
+        if page_number in self.page_table:
+            # Move page to the end of the memory - most recently used
+            self.memory.remove(page_number)
+            self.memory.append(page_number)
+            if write:
+                self.page_table[page_number].dirty = True
             return
-        # Page fault: Page is not in memory
+
+        # Page fault
+        logger.debug(f"Page fault at page {page_number}")
         self.num_page_faults += 1
         self.num_disk_reads += 1
-        logger.debug(f"Page fault on write {page_number}")
 
-        if len(self.page_table) >= self.frames:
-            self.evict_page()
+        # Evict least recently used frame
+        if len(self.memory) >= self.frames:
+            evict_page_number = self.memory.pop(0)  # Least recently used
+            evict_page = self.page_table[evict_page_number]
+            if evict_page.dirty:
+                self.num_disk_writes += 1
+                logger.debug(f"Write page {evict_page_number} to disk")
+            del self.page_table[evict_page_number]
+            logger.debug(f"Evict page {evict_page_number}")
 
-        # Find the next available frame
-        frame_number = len(self.page_table)
-        self.page_table[page_number] = frame_number
-        self.memory.append(frame_number)
-        self.dirty.append(frame_number) # This frame is marked as written - dirty
-        self.access_time[page_number] = time.time()
-        logger.debug(f"Loaded page {page_number} into frame {frame_number}")
+        # Load new page into memory
+        self.page_table[page_number] = Page(page_number, write)
+        self.memory.append(page_number)
+        logger.debug(f"Load new page {page_number}")
 
     def get_total_disk_reads(self):
         return self.num_disk_reads
